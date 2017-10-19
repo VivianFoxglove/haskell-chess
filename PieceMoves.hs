e7ad3a235857cf41ea9a31c19aa69a6d2dd23638
nudemanonbike <nudemanonbike@gmail.com>
2017-10-19 17:42:16 -0400

    King Movement

--- PieceMoves.hs
+++ PieceMoves.hs
@@ -1,8 +1,16 @@
 type PiecePos = (Int,Int)
 
-moveKnight :: PiecePos -> [PiecePos]  
-moveKnight (x,y) = filter onBoard  
-    [(x+2,y-1),(x+2,y+1),(x-2,y-1),(x-2,y+1)  
-    ,(x+1,y-2),(x+1,y+2),(x-1,y-2),(x-1,y+2)  
-    ]  
+moveKnight :: PiecePos -> [PiecePos]
+moveKnight (x,y) = filter onBoard
+    [(x+2,y-1),(x+2,y+1),(x-2,y-1),(x-2,y+1)
+    ,(x+1,y-2),(x+1,y+2),(x-1,y-2),(x-1,y+2)
+    ]
     where onBoard (x,y) = x `elem` [1..8] && y `elem` [1..8]
+
+
+moveKing :: PiecePos -> [PiecePos]
+moveKing (x,y) = filter onBoard
+    [(x+1,y+1),(x,y+1),(x+1,y),(x+1,y-1),
+    (x-1,y+1),(x-1,y-1),(x,y-1),(x-1,y)
+    ]
+    where onBoard(x,y) = x `elem` [1..8] && y `elem` [1..8]
